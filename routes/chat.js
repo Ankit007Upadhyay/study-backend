65584a1816a35d36b92283b6d1c8a0fe458c63bc
ankit <ankityay007@gmail.com>
2025-09-07 01:10:01 +0530

    fix auth middleware import destructuring in routes

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Message = require('../models/Message');
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const router = express.Router();
 
 // Get all messages (with pagination) - only messages from last 24 hours
